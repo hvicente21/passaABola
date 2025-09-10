79068fe8fcc21f2d3d9b8e0bd8fb9ba7ed5a3675
Henrique Vicente <hvicente2006@gmail.com>
2025-09-10 11:15:11 -0300

    Criando cadastro e inscricao de jogadoras

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-# preencherRequisitos()
+def negarVazio(var,msg,msgErro):
+    while len(var) < 3:
+        print(msgErro)
+        var = input(msg) 
+    return
+
+    
 def forcaOpcao(msg,listaOpcao):
     opcoes = ', '.join(listaOpcao)
     escolha = input(f"{msg}\n{opcoes}\n->")
@@ -13,16 +19,33 @@ def verificaNumero(msg):
         num = input(msg)
     return int(num)
 
+usuarios = []
+jogadoras = []
+telefones = []
+posiçoes = []
+emailJogadoras = ["goleira","zagueira","lateral-direita","lateral-esquerda","meio-campo","atacante"]
+idades = []
+times = []
+experiencias = []
+
+
+
 
 adminEmail = "admin@gmail.com"
 adminSenha = "senha123"
 print("Olá seja Bem-Vindo(a) ao Passa a Bola")
-emailUsuario = input("Digite seu email")
-senhaUsuario = input("Digite sua senha")
+emailUsuario = input("Digite seu email: ")
+senhaUsuario = input("Digite sua senha: ")
 while emailUsuario == "" or senhaUsuario == "":
     print("Preencha todos os requisitos")
-    emailUsuario = input("Digite seu email")
-    senhaUsuario = input("Digite sua senha")
+    emailUsuario = input("Digite seu email: ")
+    senhaUsuario = input("Digite sua senha: ")
+usuarios.append(emailUsuario)
 
-
-
+inscrever = input("Voce deseja iniciar a incrição? (s/n): ")
+while inscrever != "s" and "n":
+    print("Voce deve digitar s(sim) ou n(nao) ")
+    inscrever = input("Voce deseja iniciar a incrição? (s/n): ") 
+if inscrever == "s":
+    jogadora = input("Digite seu nome completo: ")
+    negarVazio(jogadora,"Digite seu nome completo","Digite um nome com mais de 2 caracteres" )
