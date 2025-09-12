6b50bf0b0ffb568d5de64aa1e2da86e2f9037d23
Henrique Vicente <hvicente2006@gmail.com>
2025-09-12 15:05:57 -0300

    Finalizando projeto, arrumando esquemas de opção

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def verificaTelefone(msg):  # telefone tinha que verificar se é um numero e ver
 def forcaOpcao(msg, listaOpcao):
    opcoes = ', '.join(listaOpcao)
    escolha = input(f"{msg}\n{opcoes}\n->")
-   while not escolha in listaOpcao:
+   while not escolha.title() in listaOpcao:
        escolha = input(f"{msg}\n{opcoes}\n->")
    return escolha
 
@@ -52,76 +52,95 @@ def maiorIdade(msg):
 
        else:
            return idade
+       
+def listarInscricoesLinha():
+    if not jogadoras:
+        print("Ainda não há jogadoras inscritas.")
+        return
+    print("\n--- Lista de Inscrições ---")
+    
+    for i in range(len(jogadoras)):
+        print(
+            f"Nome: {jogadoras[i]} | Telefone: {telefones[i]} | Posição: {posicaoJogadora[i]} | Time: {times[i]} | E-mail: {emailJogadoras[i]} | Idade: {idades[i]} | Experiência: {experienciaJogadora[i]} | Observações: {observacoes[i]}")
 
 
 usuarios = []
 jogadoras = []
 telefones = []
 emailJogadoras = []
-posicoes = ["goleira", "zagueira", "lateral-direita", "lateral-esquerda", "meio-campo", "atacante"]
+posicoes = ["Goleira", "Zagueira", "Lateral-direita", "Lateral-esquerda", "Meio-campo", "Atacante"]
 posicaoJogadora = []
 idades = []
 times = []
 experienciaJogadora= []
 experiencias = ["Iniciante","Amadora","Semi-profissional","Profissional"]
-observaçoes = []
+observacoes = []
 
 
 
 print("Olá seja Bem-Vindo(a) ao Passa a Bola")
-print("Voce sera enchaminhada para a area de inscrição de jogadoras")
-inscrever = input("Voce deseja iniciar a incrição? (s/n): ")
-while inscrever != "s" and "n":
-   print("Voce deve digitar s(sim) ou n(nao) ")
-   inscrever = input("Voce deseja iniciar a incrição? (s/n): ")
-if inscrever == "s":
+while True:
+    print("\nMENU:")
+    print("1 - Fazer Inscrição")
+    print("2 - Ver Inscrições")
+    print("3 - Sair")
+    
+    try: 
+        escolha = int(input("Escolha uma opção: "))
+
+        if escolha == 1:
+            print("Voce sera enchaminhada para a area de inscrição de jogadoras")
+            inscrever = input("Voce deseja iniciar a incrição? (s/n): ")
+            while inscrever.lower() not in ["s","n"]:
+                print("Voce deve digitar s(sim) ou n(nao) ")
+                inscrever = input("Voce deseja iniciar a incrição? (s/n): ")
+            if inscrever == "s":
+
+                jogadora = negarVazio("Digite seu nome completo: ")
+                jogadoras.append(jogadora)
 
 
-   jogadora = negarVazio("Digite seu nome completo: ")
-   jogadoras.append(jogadora)
+                telefone = verificaTelefone("Digite seu numero para contato: ")
+                telefones.append(telefone)
 
 
-   telefone = verificaTelefone("Digite seu numero para contato: ")
-   telefones.append(telefone)
+                posicao = forcaOpcao("Digite em qual posição voce joga: ", posicoes).lower()
+                posicaoJogadora.append(posicao)
+
+                time = negarVazio("Digite seu time: ")
+                times.append(time)
 
 
-   posicao = forcaOpcao("Digite em qual posição voce joga: ", posicoes)
-   posicaoJogadora.append(posicao
-                          )
-   time = negarVazio("Digite seu time: ")
-   times.append(time)
+                emailJogadora = negarVazio("Digite seu email para contato: ")
+                emailJogadoras.append(emailJogadora)
 
 
-   emailJogadora = negarVazio("Digite seu email para contato: ")
-   emailJogadoras.append(emailJogadora)
+                idade = maiorIdade("Digite seu ano de nascimento: ")
+                idades.append(idade)
 
 
-   idade = maiorIdade("Digite seu ano de nascimento: ")
-   idades.append(idade)
+                experiencia =forcaOpcao("Digite sua experiência no futebol: ", experiencias).lower()
+                experienciaJogadora.append(experiencia)
 
 
-   experiencia =forcaOpcao("Digite sua experiência no futebol: ", experiencias)
-   experienciaJogadora.append(experiencia)
+                observacao = negarVazio("Digite se voce tem alguma observação no futebol: ")
+                observacoes.append(observacao)
+
+        elif escolha == 2:
+            listarInscricoesLinha()
+
+        elif escolha == 3:
+            print("Muito obrigada, ate breve!!!")
+            break
+            
+        else: 
+            print("Escolha invalida")
+            
+    except ValueError:
+        print("Entrada inválida. Por favor, digite um número.")
 
 
-   observacao = negarVazio("Digite se voce tem alguma observação no futebol: ")
-   observaçoes.append(observacao)
 
-
-   verInscricoes = negarVazio("Voce gostaria de ver as jogadoras inscritas? (s/n): ")
-   while verInscricoes != "s" and "n":
-       print("Voce deve digitar s(sim) ou n(nao) ")
-       verInscricoes = negarVazio("Voce gostaria de ver as jogadoras inscritas? (s/n): ")
-       if verInscricoes == "s":
-           print(f"Temos inscrita a jogadora {jogadoras[0]}  {telefones[0]}, e a km é {posicaoJogadora[0]} {times[0]}, e a km é {emailJogadora[0]} {idades[0]}, e a km é {experienciaJogadora[0]} {observaçoes[0]}")
-
-
-else:
-   verInscricoes = negarVazio("Voce gostaria de ver as jogadoras inscritas? (s/n): ")
-   print("Muito obrigado, até breve!!!")
-
-
-print(jogadoras[0],telefones[0],posicaoJogadora[0],times[0],emailJogadoras[0],experienciaJogadora[0],observaçoes[0])
 
 
 
